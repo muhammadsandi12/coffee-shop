e7bc9de85f38154a0ce244629c258e66e428b948
muhammadsandi12 <muhammadsandimaulana12@gmail.com>
2022-09-26 11:48:21 +0700

    update users

--- coffee-shop/coffee-shop/redux/reducer/index.js
+++ coffee-shop/coffee-shop/redux/reducer/index.js
@@ -5,12 +5,14 @@ import Register from "./Register"
 import Login from "./Login"
 import GetUsers from "./GetUsers"
 
+
 const rootReducers = combineReducers({
     getCheckout : GetCheckout,
     addPayment: AddPayment,
     login: Login,
     register: Register,
-    getUsers: GetUsers
+    getUsers: GetUsers,
+    updateUsers: updateUsers
    
 })
 
